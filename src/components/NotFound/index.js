2e8f0a499085ac5720623273391e0ac86886ef49
venkatesh bandi <100306737+venkateshbandi08@users.noreply.github.com>
2024-02-16 04:44:31 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -5,7 +5,7 @@ import './index.css'
 const NotFound = () => (
     <div className='not-fount-bg-container'>
         <img src='https://t4.ftcdn.net/jpg/03/88/63/83/360_F_388638369_wSBADhKfhiTx6Q5Pz1xfdpy6zotku1Sg.jpg' alt='error' />
-        <Link to='/home' className='home-button'>
+        <Link to='/' className='home-button'>
             <button className='btn btn-danger'> Go to Home Page </button>
         </Link>
     </div>
